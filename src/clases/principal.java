e3222988e0ee77c3e8df6a6a90e8b4e368afc5ca
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-01-17 00:16:00 -0600

    Se añadió código para crear una notificacion de error de conexión a internet de DragShot en principal.java lineas 22-24

--- src/clases/principal.java
+++ src/clases/principal.java
@@ -4,13 +4,24 @@
  */
 package clases;
 
+//<editor-fold defaultstate="collapsed" desc="Libreria notificaciones DS">
+import Formularios.inicioSesion;
+import ds.desktop.notify.DesktopNotify;
+//</editor-fold>
+
+//<editor-fold defaultstate="collapsed" desc="Libreria para conexion a internet">
+
+//</editor-fold>
+
 /**
  *
  * @author Emmanuelito18
  */
 public class principal {
     //<editor-fold defaultstate="collapsed" desc="Notificaciones DS">
-    
+    public static void notificacionDSErrorConexion(){
+        DesktopNotify.showDesktopMessage("Error de conexión","No se ha podido conectar a internet, por favor verifique su conexión a internet",DesktopNotify.ERROR,10000L);
+    }
     //</editor-fold>
     
     //<editor-fold defaultstate="collapsed" desc="Notificaciones RS">
@@ -19,6 +30,12 @@ public class principal {
     
     //<editor-fold defaultstate="collapsed" desc="Notificaciones Java">
     
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Verificar notificacion">
+    public static boolean verificarConexionInternet(){
+     return true;
+    }
     //</editor-fold>
     
     public static void main(String[] args){
