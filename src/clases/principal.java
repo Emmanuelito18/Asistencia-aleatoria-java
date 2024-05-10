20402535c13dfbadeff194564f571800e108ba19
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-05-09 19:41:04 -0500

    -Cambios en principal.java *Se añadieron las librerias para cambiar el look and feel del programa y para cargar los archivos .properties líneas 8-14

--- src/clases/principal.java
+++ src/clases/principal.java
@@ -5,15 +5,20 @@
 package clases;
 
 import Formularios.inicioSesion;
-/**
- * Esta clase funciona como la clase principal de todo el programa
- * @author Emmanuelito18
- */
+import propiedades.idioma;//hace las traducciones del programa
+//<editor-fold defaultstate="collapsed" desc="Librerias para archivo properties y recordar de LookAndFeel">
+import java.util.Properties;
+import javax.swing.UIManager;
+import java.io.FileInputStream;
+import java.io.IOException;
+//</editor-fold>
+
 public class principal {
     /*Aquí estarán todos los métodos que utilizarán las demás
     clases del programa, con el fin de optimizar todo el programa y
     hacerlo máa legible y fácil de entender el código del mismo
     */
+    
     public static void main(String[] args){
         inicioSesion inicio=new inicioSesion();
         inicio.setVisible(true);
