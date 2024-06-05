c0e7d0438cd293ec073b55ac812314170f2d8f47
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-06-05 01:04:21 -0600

    -Cambios en verificaConexionInternet.java *Se añadieron comentarios al código de la conexión a google linéas 20-21

--- src/clases/verificaConexionInternet.java
+++ src/clases/verificaConexionInternet.java
@@ -17,8 +17,8 @@ public class verificaConexionInternet {
     public static boolean verificarConexionGoogle(){
         try(Socket socket=new Socket()){
             int tiempoLimite=2000;//tiempo de espera en milisegundos
-            SocketAddress socketGoogle=new InetSocketAddress("www.google.com",80);
-            socket.connect(socketGoogle, tiempoLimite);
+            SocketAddress socketGoogle=new InetSocketAddress("www.google.com",80);//crea socket con la direción de google con el puerto 80
+            socket.connect(socketGoogle, tiempoLimite);//se conecta a un websocket con una dirección, puerto y tiempo limite especificados
             return true;
         }catch(IOException error){
             return false;
