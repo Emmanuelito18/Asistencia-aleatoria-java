c588906e17b9bb7c499f11bb5653bb06466c14cb
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-06-04 23:48:28 -0600

    -Cambios en verificaConexionInternet.java *Se añadieron las librerias necesarias para conectarse a interne utilizando socketAddress líneas 3-9

--- src/clases/verificaConexionInternet.java
+++ src/clases/verificaConexionInternet.java
@@ -1,5 +1,13 @@
 package clases;
 
+//<editor-fold defaultstate="collapsed" desc="Librerias para conetarse a internet">
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+//</editor-fold>
+
 /**
  * Esta clase se encarga de verificar si el usuario tiene conexión a internet o no
  * @author Emmanuelito18
