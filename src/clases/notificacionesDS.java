eac4c9cd55f013273f3653c30be28564d5af6830
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-06-06 15:49:42 -0600

    -Cambios en notifiacionesDS.java *Se añadieron métodos que invocan a las notificaciones de la libreria de Dragshot Desktop Notify líneas 12-14,16-18,20-22,24-26,28-30,32-34

--- src/clases/notificacionesDS.java
+++ src/clases/notificacionesDS.java
@@ -1,10 +1,35 @@
 package clases;
 
+import ds.desktop.notify.DesktopNotify;
+
 /**
  * Esta clase se encarga de manejar las notificaciones de la libreria DS Desktop Notify
  * @author Emmanuelito18
- * @see <a href="https://dragshot.webcindario.com/software/desktopnotify/javadoc/overview-summary.html">Documentacion de DS Desktop NOtify</a>
+ * @author Dragshot
+ * @see <a href="https://dragshot.webcindario.com/software/desktopnotify/javadoc/overview-summary.html">Documentacion de DS Desktop Notify</a>
  */
 public class notificacionesDS {
-
+    public void notificacionError(String titulo,String mensaje){
+        DesktopNotify.showDesktopMessage(titulo, mensaje, DesktopNotify.ERROR);
+    }
+    
+    public void notificacionFallo(String titulo,String mensaje){
+        DesktopNotify.showDesktopMessage(titulo, mensaje, DesktopNotify.FAIL);
+    }
+    
+    public void notificacionExito(String titulo,String mensaje){
+        DesktopNotify.showDesktopMessage(titulo, mensaje, DesktopNotify.SUCCESS);
+    }
+    
+    public void notificacionAdvertencia(String titulo,String mensaje){
+        DesktopNotify.showDesktopMessage(titulo, mensaje, DesktopNotify.WARNING);
+    }
+    
+    public void notificacionInformacion(String titulo,String mensaje){
+        DesktopNotify.showDesktopMessage(titulo, mensaje, DesktopNotify.INFORMATION);
+    }
+    
+    public void notificacionAyuda(String titulo,String mensaje){
+        DesktopNotify.showDesktopMessage(titulo, mensaje, DesktopNotify.HELP);
+    }
 }
