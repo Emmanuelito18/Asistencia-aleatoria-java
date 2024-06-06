b282171c8a4fcc6806c8bf854ce7ca972dd941de
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-06-06 15:09:40 -0600

    -Cambios en notifiacionesDS.java *Se añadio comentarios javadoc a la clase líneas 3,6

--- src/clases/notificacionesDS.java
+++ src/clases/notificacionesDS.java
@@ -1,7 +1,9 @@
 package clases;
 
 /**
+ * Esta clase se encarga de manejar las notificaciones de la libreria DS Desktop Notify
  * @author Emmanuelito18
+ * @see <a href="https://dragshot.webcindario.com/software/desktopnotify/javadoc/overview-summary.html">Documentacion de DS Desktop NOtify</a>
  */
 public class notificacionesDS {
 
